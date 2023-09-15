243ec5f11a06332a4ed4d2cc52317fa2ca06bf80
Eslam Abdelmaqsoud <eslamabdelmaqsoud8@gmail.com>
2023-09-16 01:05:15 +0300

    Initial commit

---
+++ app/src/main/java/com/alyndroid/facebookv2/ui/main/MainActivity.java
@@ -0,0 +1,50 @@
+package com.alyndroid.facebookv2.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.alyndroid.facebookv2.R;
+import com.alyndroid.facebookv2.pojo.PostModel;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    // MVVM //
+    PostViewModel postViewModel;
+    // MVVM //
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // MVVM //
+        postViewModel = ViewModelProviders.of(this).get(PostViewModel.class);
+        postViewModel.getPosts();
+        // MVVM //
+
+
+        //// RecyclerView ///
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        final PostsAdapter adapter = new PostsAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+        //// RecyclerView ///
+
+
+        postViewModel.postsMutableLiveData.observe(this, new Observer<List<PostModel>>() {
+            @Override
+            public void onChanged(List<PostModel> postModels) {
+                adapter.setList(postModels);
+            }
+        });
+
+    }
+}
